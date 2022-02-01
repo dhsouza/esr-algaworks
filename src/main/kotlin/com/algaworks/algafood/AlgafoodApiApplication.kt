ecd6f6e09844294e3b9dfb1c85147f04605ce52e
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2022-02-01 13:47:22 -0300

    2.5. Criando um projeto Spring Boot com Spring Initializr

---
+++ src/main/kotlin/com/algaworks/algafood/AlgafoodApiApplication.kt
@@ -0,0 +1,11 @@
+package com.algaworks.algafood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgafoodApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlgafoodApiApplication>(*args)
+}
