39c4445e628ef022df608c24600affdd3a80ea68
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2022-04-24 21:41:27 -0300

    7.6. Adicionando o Flyway no projeto e criando a primeira migração

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("mysql:mysql-connector-java")
+    implementation("org.flywaydb:flyway-core")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
