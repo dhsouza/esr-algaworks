ddcc518ae62427db9844d02eb33a077d088c299a
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2022-04-25 13:58:26 -0300

    7.10. Adicionando dados de testes com callback do Flyway

--- src/main/kotlin/com/algaworks/algafood/domain/repository/RestauranteRepository.kt
+++ src/main/kotlin/com/algaworks/algafood/domain/repository/RestauranteRepository.kt
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query
 
 interface RestauranteRepository : CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante>{
 
-    @Query("select distinct r from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    @Query("select distinct r from Restaurante r join fetch r.cozinha")
     override fun findAll(): List<Restaurante>
 }
--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,3 +1,30 @@
+set foreign_key_checks = 0;
+
+delete from cidade;
+delete from cozinha;
+delete from estado;
+delete from forma_pagamento;
+delete from grupo;
+delete from grupo_permissao;
+delete from permissao;
+delete from produto;
+delete from restaurante;
+delete from restaurante_forma_pagamento;
+delete from usuario;
+delete from usuario_grupo;
+
+set foreign_key_checks = 1;
+
+alter table cidade auto_increment = 1;
+alter table cozinha auto_increment = 1;
+alter table estado auto_increment = 1;
+alter table forma_pagamento auto_increment = 1;
+alter table grupo auto_increment = 1;
+alter table permissao auto_increment = 1;
+alter table produto auto_increment = 1;
+alter table restaurante auto_increment = 1;
+alter table usuario auto_increment = 1;
+
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 insert into cozinha (id, nome) values (3, 'Argentina');
