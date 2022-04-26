2062d2a5cab183426a638664aebfccf94606ad70
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2022-04-26 19:00:33 -0300

    8.4. Estendendo ResponseStatusException

--- src/main/kotlin/com/algaworks/algafood/domain/exceptions/EntidadeNaoEncontradaException.kt
+++ src/main/kotlin/com/algaworks/algafood/domain/exceptions/EntidadeNaoEncontradaException.kt
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.domain.exceptions
 
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.server.ResponseStatusException
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-data class EntidadeNaoEncontradaException(val mensagem: String) : RuntimeException(mensagem)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
+class EntidadeNaoEncontradaException(status: HttpStatus, mensagem: String) : ResponseStatusException(status, mensagem) {
+    constructor(mensagem: String) : this(HttpStatus.NOT_FOUND, mensagem)
+}
