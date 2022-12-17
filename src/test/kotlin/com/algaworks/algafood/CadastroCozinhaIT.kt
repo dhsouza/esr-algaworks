6f3256c34e309bd5f58e51a6859fd3c0de5b4a68
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2022-12-16 22:49:47 -0300

    10.9. Validando o corpo da resposta HTTP

--- src/test/kotlin/com/algaworks/algafood/CadastroCozinhaIT.kt
+++ src/test/kotlin/com/algaworks/algafood/CadastroCozinhaIT.kt
@@ -1,7 +1,10 @@
 package com.algaworks.algafood
 
+import com.algaworks.algafood.domain.model.Cozinha
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import org.hamcrest.Matchers.hasItems
+import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -26,5 +29,18 @@ class CadastroCozinhaIT {
             .get()
 		.then()
             .statusCode(HttpStatus.OK.value())
+    }
+
+    @Test
+    fun `when finding cozinhas should return four cozinhas`() {
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .`when`()
+            .get()
+        .then()
+            .body("", hasSize<Cozinha>(4))
+            .body("nome", hasItems("Indiana", "Tailandesa"))
     }
 }
