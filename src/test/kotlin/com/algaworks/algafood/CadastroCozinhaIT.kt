e79ef6e76eea032fe7979af32506159d6877f100
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2022-12-16 23:09:09 -0300

    10.13. Configurando um banco de testes e usando @TestPropertySource

--- src/test/kotlin/com/algaworks/algafood/CadastroCozinhaIT.kt
+++ src/test/kotlin/com/algaworks/algafood/CadastroCozinhaIT.kt
@@ -1,29 +1,45 @@
 package com.algaworks.algafood
 
 import com.algaworks.algafood.domain.model.Cozinha
+import io.restassured.RestAssured
+import io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
-import org.hamcrest.Matchers.hasItems
+import org.flywaydb.core.Flyway
 import org.hamcrest.Matchers.hasSize
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 
     @LocalServerPort
     private var port: Int = 0
 
+    @Autowired
+    private lateinit var flyway: Flyway
+
+    @BeforeEach
+    fun setup() {
+        enableLoggingOfRequestAndResponseIfValidationFails()
+        RestAssured.port = port
+        RestAssured.basePath = "/cozinhas"
+
+        flyway.migrate()
+    }
+
     @Test
     fun `when finding cozinhas should return 200 ok`() {
         given()
-            .basePath("/cozinhas")
-            .port(port)
             .accept(ContentType.JSON)
 		.`when`()
             .get()
@@ -34,13 +50,22 @@ class CadastroCozinhaIT {
     @Test
     fun `when finding cozinhas should return four cozinhas`() {
         given()
-            .basePath("/cozinhas")
-            .port(port)
             .accept(ContentType.JSON)
         .`when`()
             .get()
         .then()
             .body("", hasSize<Cozinha>(4))
-            .body("nome", hasItems("Indiana", "Tailandesa"))
+    }
+
+    @Test
+    fun `when saving a cozinha should return 201 created`() {
+        given()
+            .body("{ \"nome\": \"Chinesa\" }")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .`when`()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value())
     }
 }
